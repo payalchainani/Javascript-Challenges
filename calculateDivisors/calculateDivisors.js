5c0f3eff222fdc6c9ec8e69ca415071817c192ee
Harriet <harriethryder@gmail.com>
2017-11-18 05:00:49 +0000

    Katapult calculateDivisors

---
+++ calculateDivisors/calculateDivisors.js
@@ -0,0 +1,8 @@
+// Please do not change the name of this function
+var calculateDivisors = function (number) {
+  // Your code here
+};
+
+if (typeof module !== 'undefined') {
+  module.exports = {calculateDivisors};
+}
