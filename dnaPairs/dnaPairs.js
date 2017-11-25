17e2b8b02dae6487b3eaa6c1deb74e47142d7ce1
Harriet <harriethryder@gmail.com>
2017-11-25 05:00:50 +0000

    Katapult dnaPairs

---
+++ dnaPairs/dnaPairs.js
@@ -0,0 +1,8 @@
+// Please do not change the name of this function
+var dnaPairs = function (dna) {
+  // Your code here
+};
+
+if (typeof module !== 'undefined') {
+  module.exports = {dnaPairs};
+}
---
+++ dnaPairs/spec/dnaPairs.spec.js
@@ -0,0 +1,12 @@
+var path = require('path');
+var expect = require('chai').expect;
+
+var {dnaPairs} = require(path.join(__dirname, '..', './dnaPairs.js'));
+
+describe('dnaPairs()', function () {
+  'use strict';
+
+  it('ADD FIRST TEST AND RENAME', function() {
+
+  });
+});
