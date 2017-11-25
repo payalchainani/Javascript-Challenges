77749e40651bf59fc53b34a345be31f4a5d7fa2d
Harriet <harriethryder@gmail.com>
2017-11-25 05:00:54 +0000

    Katapult getTweetData

---
+++ getTweetData/spec/getTweetData.spec.js
@@ -0,0 +1,14 @@
+var path = require('path');
+var expect = require('chai').expect;
+
+var {getTweetData} = require(path.join(__dirname, '..', './getTweetData.js'));
+
+describe('getTweetData()', function () {
+  'use strict';
+
+  it('ADD TEST AND RENAME', function () {
+
+  });
+
+
+});
