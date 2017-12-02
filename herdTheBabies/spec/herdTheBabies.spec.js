376cc93737d78ea6fa6b9123b019d3c86b827ec7
Harriet <harriethryder@gmail.com>
2017-12-02 05:00:50 +0000

    Katapult herdTheBabies

---
+++ herdTheBabies/spec/herdTheBabies.spec.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect;
+const {herdTheBabies} = require('../herdTheBabies');
+
+describe('herdTheBabies()', function () {
+  it('ADD TEST AND RENAME', function () {
+    
+  });
+});
