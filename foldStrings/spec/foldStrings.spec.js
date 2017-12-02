76169178e321aa91c07b64b9f956fb6095666f00
Harriet <harriethryder@gmail.com>
2017-12-02 05:00:55 +0000

    Katapult foldStrings

---
+++ foldStrings/spec/foldStrings.spec.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+var {foldString} = require('../foldStrings');
+
+describe('foldString', function () {
+  it('ADD TEST AND RENAME', function() {
+    
+  });
+});
