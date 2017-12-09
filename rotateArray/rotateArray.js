71e858b1d49da8ae82a9f3961209a62951f95242
Harriet <harriethryder@gmail.com>
2017-12-09 05:00:55 +0000

    Katapult rotateArray

---
+++ rotateArray/rotateArray.js
@@ -0,0 +1,8 @@
+// Please do not change the name of this function
+var rotateArray = function (arr, num) {
+  // your code goes here
+};
+
+if ( typeof module !== 'undefined' ) {
+  module.exports = {rotateArray};
+}
---
+++ rotateArray/spec/rotateArray.spec.js
@@ -0,0 +1,12 @@
+var path = require('path');
+var expect = require('chai').expect;
+
+var {rotateArray} = require(path.join(__dirname, '..', './rotateArray.js'));
+
+describe('rotateArray()', function () {
+  'use strict';
+
+  it('ADD TEST AND RENAME', function () {
+    
+  });
+});
