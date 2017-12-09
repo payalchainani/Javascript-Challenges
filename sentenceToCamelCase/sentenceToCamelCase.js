8c9d1619251df0e8c2277c64d4b3562ddf256312
Harriet <harriethryder@gmail.com>
2017-12-09 05:00:50 +0000

    Katapult sentenceToCamelCase

---
+++ sentenceToCamelCase/sentenceToCamelCase.js
@@ -0,0 +1,8 @@
+// Please do not change the name of this function
+var sentenceToCamelCase = function () {
+  // your code goes here
+};
+
+if ( typeof module !== 'undefined' ) {
+  module.exports = {sentenceToCamelCase};
+}
---
+++ sentenceToCamelCase/spec/sentenceToCamelCase.spec.js
@@ -0,0 +1,14 @@
+var path = require('path');
+var expect = require('chai').expect;
+
+var {sentenceToCamelCase} = require(path.join(__dirname, '..', './sentenceToCamelCase.js'));
+
+describe('sentenceToCamelCase()', function () {
+  'use strict';
+
+  it('ADD TEST AND RENAME', function () {
+    
+  });
+
+  // Add more assertions here
+});
