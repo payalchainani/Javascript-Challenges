d3f182e18e5077d581d7b4cb586e481e6d05357d
Harriet <harriethryder@gmail.com>
2017-12-16 05:00:50 +0000

    Katapult tillAddition

---
+++ tillAddition/spec/tillAddition.spec.js
@@ -0,0 +1,13 @@
+var path = require('path');
+var expect = require('chai').expect;
+
+var {tillAddition} = require(path.join(__dirname, '..', './tillAddition.js'));
+
+describe('tillAddition()', function () {
+  'use strict';
+
+  it('RENAME AND ADD TEST', function () {
+    
+  });
+  // Add more assertions here
+});
