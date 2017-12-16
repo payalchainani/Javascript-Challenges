0338ec5b49553b98bedf636ec1081a91261eaf6b
Harriet <harriethryder@gmail.com>
2017-12-16 05:00:55 +0000

    Katapult secondsToTimeString

---
+++ secondsToTimeString/secondsToTimeString.js
@@ -0,0 +1,8 @@
+// Please do not change the name of this function
+function secondsToTimeString(seconds) {
+
+}
+
+if ( typeof module !== 'undefined' ) {
+  module.exports = {secondsToTimeString};
+}
---
+++ secondsToTimeString/spec/secondsToTimeString.spec.js
@@ -0,0 +1,9 @@
+var path = require('path');
+var expect = require('chai').expect;
+
+var {secondsToTimeString} = require(path.join(__dirname, '..', './secondsToTimeString.js'));
+
+describe('secondsToTimeString()', function () {
+  'use strict';
+
+});
