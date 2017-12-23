6a7c35dde2a2c6dcaf1e8232abeed39d2fb2a32a
Harriet <harriethryder@gmail.com>
2017-12-23 05:00:54 +0000

    Katapult counterIntelligence

---
+++ counterIntelligence/counterIntelligence.js
@@ -0,0 +1,7 @@
+// Please do not change the name of this function
+
+function counterIntelligence () {
+
+}
+
+module.exports = {counterIntelligence};
---
+++ counterIntelligence/spec/counterIntelligence.spec.js
@@ -0,0 +1,6 @@
+const expect = require('chai').expect;
+const {counterIntelligence} = require('../counterIntelligence');
+
+describe('counterIntelligence()', function () {
+
+});
