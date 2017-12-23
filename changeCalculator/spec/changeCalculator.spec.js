58b90460a56a3d08631e64ca08b73daca3c1a629
Harriet <harriethryder@gmail.com>
2017-12-23 05:00:49 +0000

    Katapult changeCalculator

---
+++ changeCalculator/spec/changeCalculator.spec.js
@@ -0,0 +1,12 @@
+var expect = require('chai').expect;
+
+var {changeCalculator} = require('../changeCalculator');
+
+describe('changeCalculator()', function () {
+  'use strict';
+
+  it('ADD TEST AND RENAME', function () {
+    
+  });
+
+});
