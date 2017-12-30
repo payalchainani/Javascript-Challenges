da7b8157a5582300e8f36f52108eb9772a2c78d9
Harriet <harriethryder@gmail.com>
2017-12-30 05:00:50 +0000

    Katapult counterSpy

---
+++ counterSpy/counterSpy.js
@@ -0,0 +1,7 @@
+// Please do not change the name of this function
+
+function counterSpy () {
+
+}
+
+module.exports = {counterSpy};
---
+++ counterSpy/spec/counterSpy.spec.js
@@ -0,0 +1,6 @@
+const expect = require('chai').expect;
+const {counterSpy} = require('../counterSpy');
+
+describe('counterSpy()', function () {
+
+});
