2135d4b90631ae7958001b287901d5ee08fc4766
Harriet <harriethryder@gmail.com>
2018-01-06 06:00:55 +0100

    Katapult morseCode

---
+++ morseCode/morseCode.js
@@ -0,0 +1,8 @@
+// Please do not change the name of this function
+var morseDecode = function () {
+  // your code goes here
+};
+
+if ( typeof module !== 'undefined' ) {
+  module.exports = {morseDecode};
+}
---
+++ morseCode/spec/morseCode.spec.js
@@ -0,0 +1,9 @@
+var path = require('path');
+var expect = require('chai').expect;
+
+var {morseDecode} = require(path.join(__dirname, '..', './morseCode.js'));
+
+describe('morseDecode()', function () {
+  'use strict';
+  
+});
