5e9efd97d4c996ae51d0f43d8d675ffb13ccfb71
Harriet <harriethryder@gmail.com>
2018-01-06 06:00:50 +0100

    Katapult validTriangles

---
+++ validTriangles/spec/validTriangles.spec.js
@@ -0,0 +1,7 @@
+const expect = require('chai').expect;
+const {validTriangles} = require('../validTriangles');
+
+describe('validTriangles', function () {
+
+
+});
---
+++ validTriangles/validTriangles.js
@@ -0,0 +1,6 @@
+// Please do not change the name of this function
+function validTriangles () {
+
+}
+
+module.exports = {validTriangles};
