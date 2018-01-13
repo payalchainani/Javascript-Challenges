38fa7cba447d9442b50faeef4f88be20b2bb8941
Harriet <harriethryder@gmail.com>
2018-01-13 05:00:55 +0000

    Katapult findMissingLetter

---
+++ findMissingLetter/findMissingLetter.js
@@ -0,0 +1,8 @@
+// Please do not rename this function
+var findMissingLetter = function () {
+  // Your code here
+};
+
+if (typeof module !== 'undefined') {
+  module.exports = {findMissingLetter};
+}
---
+++ findMissingLetter/spec/findMissingLetter.spec.js
@@ -0,0 +1,9 @@
+var path = require('path');
+var expect = require('chai').expect;
+
+var {findMissingLetter} = require(path.join(__dirname, '..', 'findMissingLetter.js'));
+
+describe('findMissingLetter()', function () {
+  'use strict';
+  
+});
