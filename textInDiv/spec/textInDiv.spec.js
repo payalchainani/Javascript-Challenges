2ed4d5cb8759089def4698293f6c54807f0238af
Harriet <harriethryder@gmail.com>
2018-01-20 05:00:55 +0000

    Katapult textInDiv

---
+++ textInDiv/spec/textInDiv.spec.js
@@ -0,0 +1,9 @@
+var path = require('path');
+var expect = require('chai').expect;
+
+var {textInDiv} = require(path.join(__dirname, '..', './textInDiv.js'));
+
+describe('textInDiv()', function () {
+  'use strict';
+
+});
