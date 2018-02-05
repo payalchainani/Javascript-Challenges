91042cf2ef92eae7ad78bd8a2bf8f411e9067eb3
Payal <payal_12@hotmail.com>
2018-02-05 09:49:55 +0000

    completed foldArray with working tests

--- foldArray/foldArray.js
+++ foldArray/foldArray.js
@@ -1,5 +1,30 @@
 // Please do not rename this function
-function foldArray () {
+function foldArray(arr, n = 1) {
+
+    if (n === 0) {
+        return arr.reverse();
+    }
+    if (Array.isArray(arr) === false) {
+        return [];
+    }
+    
+    let front = arr.slice(arr.length / 2);
+    let back = arr.reverse().slice(arr.length / 2);
+
+    
+    if (arr.length % 2 === 1) {
+        var middle = front.shift();
+        back.shift();
+    }
+    
+    var final = front.map(function (item, i) {
+        return item + back[i];
+    });
+    // console.log(final);
+    if (middle) {
+        final.splice(0, 0, middle);
+    }
+    return foldArray(final, n - 1);
 
 }
 
--- foldArray/spec/foldArray.spec.js
+++ foldArray/spec/foldArray.spec.js
@@ -3,4 +3,24 @@ const {foldArray} = require('../foldArray');
 
 describe('foldArray', function () {
 
+    it('should return an array', function () {
+        expect(Array.isArray(foldArray())).to.equal(true);
+    });
+        
+    it('should return a folded even array 1 time', function () {
+        expect(foldArray([1, 2])).to.eql([3]);
+    });
+
+    it('should return a folded array 1 time', function () {
+        expect(foldArray([1, 2, 3])).to.eql([4, 2]);
+        expect(foldArray([1,2,3,4,5])).to.eql([6,6,3]);
+        expect(foldArray([1, 2, 3, 10, 34, 100])).to.eql([101, 36, 13]);
+    });
+
+    it('should return a folded array n times', function () {
+        expect(foldArray([1, 2, 3, 10, 34, 100], 2)).to.eql([114, 36]);
+        expect(foldArray([1, 2, 3, 10, 34, 100], 3)).to.eql([150]);
+        expect(foldArray([1, 2, 3, 10, 34, 100], 4)).to.eql([150]);
+        expect(foldArray([1, 2, 3, 4, 5, 6, 7, 8, 9], 2)).to.eql([15, 20, 10]);
+    });
 });
