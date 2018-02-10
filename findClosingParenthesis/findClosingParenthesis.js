c958fda4e2a53c81f50a66cf0e880cb13cbd944e
Harriet <harriethryder@gmail.com>
2018-02-10 05:00:54 +0000

    Katapult findClosingParenthesis

---
+++ findClosingParenthesis/findClosingParenthesis.js
@@ -0,0 +1,8 @@
+// Please do not change the name of this function
+var findClosingParenthesis = function (str, n) {
+  
+};
+
+if (typeof module !== 'undefined') {
+  module.exports = {findClosingParenthesis};
+}
---
+++ findClosingParenthesis/spec/findClosingParenthesis.spec.js
@@ -0,0 +1,9 @@
+var path = require('path');
+var expect = require('chai').expect;
+
+var {findClosingParenthesis} = require(path.join(__dirname, '..', './findClosingParenthesis.js'));
+
+describe('findClosingParenthesis()', function () {
+  'use strict';
+  
+});
