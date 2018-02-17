6b3706b4d14d89ae0c49b2180b14fd8ae48811b6
Harriet <harriethryder@gmail.com>
2018-02-17 05:00:54 +0000

    Katapult romanNumeralEncoder

---
+++ romanNumeralEncoder/romanNumeralEncoder.js
@@ -0,0 +1,8 @@
+// Please do not change the name of this function
+var romanNumeralEncoder = function () {
+  // Your code here
+};
+
+if (typeof module !== 'undefined') {
+  module.exports = {romanNumeralEncoder};
+}
---
+++ romanNumeralEncoder/spec/romanNumeralEncoder.spec.js
@@ -0,0 +1,9 @@
+var path = require('path');
+var expect = require('chai').expect;
+
+var {romanNumeralEncoder} = require(path.join(__dirname, '..', './romanNumeralEncoder.js'));
+
+describe('romanNumeralEncoder()', function () {
+  'use strict';
+
+});
