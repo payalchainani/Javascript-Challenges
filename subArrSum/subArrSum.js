e958ab6a17f2dcd1f9ee0f509e16dd4464edc5c0
Payal <payal_12@hotmail.com>
2018-02-17 08:34:23 +0000

    subArrSum completed with working tests

--- subArrSum/subArrSum.js
+++ subArrSum/subArrSum.js
@@ -1,13 +1,13 @@
 // Please do not change the name of this function
 function subArrSum(arr) {
     let currentMaximum = 0;
-    let max = 0;
+    let maxToBereturned = 0;
 
     for (let i = 0; i < arr.length; i += 1) {
         currentMaximum = Math.max(0, currentMaximum + arr[i]);
-        max = Math.max(max, currentMaximum);
+        maxToBereturned = Math.max(maxToBereturned, currentMaximum);
     }
-    return max;
+    return maxToBereturned;
 }
 
 module.exports = {subArrSum};
