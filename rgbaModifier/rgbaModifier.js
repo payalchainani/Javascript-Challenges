bd2c96a7cfc94bf0be349dd558e8cdfb85e4fa76
Northcoder <36270882+northcoder-dev@users.noreply.github.com>
2018-02-24 05:00:54 +0000

    Katapult rgbaModifier

---
+++ rgbaModifier/rgbaModifier.js
@@ -0,0 +1,8 @@
+// Please do not change the name of this function
+var rgbaModifier = function (rgbaStr, percent, opacity) {
+  
+};
+
+if (typeof module !== 'undefined') {
+  module.exports = {rgbaModifier};
+}
---
+++ rgbaModifier/spec/rgbaModifier.spec.js
@@ -0,0 +1,9 @@
+var path = require('path');
+var expect = require('chai').expect;
+
+var {rgbaModifier} = require(path.join(__dirname, '..', './rgbaModifier.js'));
+
+describe('rgbaModifier()', function () {
+  'use strict';
+
+});
