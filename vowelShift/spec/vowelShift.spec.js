9fe08fc2a62419585a33b537f1d445d1a968d26b
Northcoder <36270882+northcoder-dev@users.noreply.github.com>
2018-02-24 05:00:49 +0000

    Katapult vowelShift

---
+++ vowelShift/spec/vowelShift.spec.js
@@ -0,0 +1,9 @@
+var path = require('path');
+var expect = require('chai').expect;
+
+var {vowelShift} = require(path.join(__dirname, '..', './vowelShift.js'));
+
+describe('vowelShift()', function () {
+  'use strict';
+
+});
