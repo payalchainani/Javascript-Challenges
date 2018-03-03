2bd28f378e2abd4c3718c3a5fcd474874d2d2f7c
Northcoder <36270882+northcoder-dev@users.noreply.github.com>
2018-03-03 05:00:49 +0000

    Katapult findPartner

---
+++ findPartner/spec/findPartner.spec.js
@@ -0,0 +1,8 @@
+var path = require('path');
+var expect = require('chai').expect;
+
+var {findPartner} = require(path.join(__dirname, '..', 'findPartner.js'));
+
+describe('findPartner()', function () {
+  'use strict';
+});
